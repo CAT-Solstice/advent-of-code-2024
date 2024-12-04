a1aff04ddc2a903b3f2b4619958735011dafd8e3
solstice <solstice@archimede.edu>
2024-12-04 22:20:38 +0100

    day_04_part_one

---
+++ examples/day_04/main.rs
@@ -0,0 +1,95 @@
+use advent_of_code_2024::Mat2D;
+
+pub fn main() {
+  let input = include_str!( "day_04.input" );
+  let answer = part_one::compute_answer( input, (140,140) );
+  println!( "{answer}" );
+}
+
+fn parse_input( input: &str ) -> Mat2D<char> {
+  Mat2D::from_iter_of_iter(
+     input.lines().map( str::chars )
+  )
+}
+
+mod part_one {
+  use super::*;
+
+  fn count_at( mat: &Mat2D<char>, row_offset: usize, col_offset: usize ) -> usize {
+    const XMAS: [Option<&char>; 4] = [Some(&'X'), Some(&'M'), Some(&'A'), Some(&'S')];
+
+    let get = move |row: usize, col: usize| -> Option<&char> {
+      mat.get(( row+row_offset, col+col_offset ))
+    };
+
+    let mut count = 0;
+    [get(0,0), get(0,1), get(0,2), get(0,3)].eq( &XMAS ).then( || count += 1 );
+    [get(0,3), get(0,2), get(0,1), get(0,0)].eq( &XMAS ).then( || count += 1 );
+
+    [get(0,0), get(1,0), get(2,0), get(3,0)].eq( &XMAS ).then( || count += 1 );
+    [get(3,0), get(2,0), get(1,0), get(0,0)].eq( &XMAS ).then( || count += 1 );
+
+    [get(0,0), get(1,1), get(2,2), get(3,3)].eq( &XMAS ).then( || count += 1 );
+    [get(3,3), get(2,2), get(1,1), get(0,0)].eq( &XMAS ).then( || count += 1 );
+
+    [get(0,3), get(1,2), get(2,1), get(3,0)].eq( &XMAS ).then( || count += 1 );
+    [get(3,0), get(2,1), get(1,2), get(0,3)].eq( &XMAS ).then( || count += 1 );
+
+    count
+  }
+
+  pub(super) fn compute_answer( input: &str, dim: (usize, usize) ) -> usize {
+    let (rows, cols) = dim;
+    let mat = parse_input( input );
+    let mut count = 0;
+    for row in 0..rows {
+      for col in 0..cols {
+        count += count_at( &mat, row, col );
+      }
+    }
+    count
+  }
+
+  #[cfg(test)]
+  mod tests {
+    use super::*;
+    use super::super::tests::TEST_INPUT;
+
+    #[test]
+    fn test_count_at() {
+      let mat = parse_input( TEST_INPUT );
+      let count = count_at( &mat, 0, 5 );
+      assert_eq!( 1, count );
+      let count = count_at( &mat, 1, 6 );
+      assert_eq!( 1, count );
+    }
+
+    #[test]
+    fn test_compute_answer() {
+      let count = compute_answer( TEST_INPUT, (10,10) );
+      assert_eq!( 18, count );
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  pub(super) const TEST_INPUT: &str = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
+
+  #[test]
+  fn test_input() {
+    let mat = parse_input( TEST_INPUT );
+    assert_eq!( Some(&'M'), mat.get((4, 1)) );
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,16 @@ use std::{fs, io, path};
 // cargo test --example day_01 part_one -- --nocapture
 // cargo test --example '*'
 
+// ---------------------------------------------------------------------------------------------------------------------------------
+
 pub fn read_file_lines<P>( path: P ) -> io::Result<impl Iterator<Item=io::Result<String>>> where P: AsRef<path::Path> {
 
   fs::File::open( path )
     .map( io::BufReader::new )
     .map( io::BufRead::lines )
 }
+
+// ---------------------------------------------------------------------------------------------------------------------------------
 
 use std::{sync::OnceLock, ops::Deref};
 
@@ -33,4 +37,57 @@ impl<T> Deref for Lazy<T> {
   fn deref( &self ) -> &Self::Target {
     self.value.get_or_init( self.supplier )
   }
+}
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+pub struct Mat2D<T> {
+  data: Vec<Vec<T>>,
+}
+
+impl<T> Mat2D<T> {
+  pub fn from_iter_of_iter<II, III>( src: III ) -> Self
+  where II: IntoIterator<Item=T>,
+        III: IntoIterator<Item=II> {
+    let data = src.into_iter()
+      .map( |line| line.into_iter().collect() )
+      .collect();
+    Self { data }
+  }
+}
+
+impl<T> Mat2D<T> {
+  pub fn get( &self, (row, col): (usize, usize) ) -> Option<&T> {
+    self.data.get( row )
+      .and_then( |row| row.get(col) )
+  }
+}
+
+// =================================================================================================================================
+
+#[cfg(test)]
+mod tests {
+
+  use super::*;
+
+  #[test]
+  fn test_mat2d() {
+    let data =
+     "aaaaaaaa
+bbbbbbbb
+cccccccc
+dddddddd
+eeeeeeee
+ffffffff
+gggggggg
+hhhhhhhh";
+
+    let mat = Mat2D::from_iter_of_iter( data.lines().map( str::chars ) );
+
+    assert_eq!( Some(&'a'), mat.get((0,0)) );
+    assert_eq!( Some(&'a'), mat.get((0,7)) );
+    assert_eq!( Some(&'h'), mat.get((7,0)) );
+    assert_eq!( Some(&'h'), mat.get((7,7)) );
+  }
+
 }
