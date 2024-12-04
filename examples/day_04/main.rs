81ce533a0095044d39a05529dcdb20fb4f8f8b73
solstice <solstice@archimede.edu>
2024-12-04 22:34:39 +0100

    day_04_part_two

--- examples/day_04/main.rs
+++ examples/day_04/main.rs
@@ -4,6 +4,8 @@ pub fn main() {
   let input = include_str!( "day_04.input" );
   let answer = part_one::compute_answer( input, (140,140) );
   println!( "{answer}" );
+  let answer = part_two::compute_answer( input, (140,140) );
+  println!( "{answer}" );
 }
 
 fn parse_input( input: &str ) -> Mat2D<char> {
@@ -68,6 +70,56 @@ mod part_one {
     fn test_compute_answer() {
       let count = compute_answer( TEST_INPUT, (10,10) );
       assert_eq!( 18, count );
+    }
+  }
+}
+
+mod part_two {
+  use super::*;
+
+  fn count_at( mat: &Mat2D<char>, row_offset: usize, col_offset: usize ) -> usize {
+    const MAS: [Option<&char>; 3] = [Some(&'M'), Some(&'A'), Some(&'S')];
+
+    let get = move |row: usize, col: usize| -> Option<&char> {
+      mat.get(( row+row_offset, col+col_offset ))
+    };
+
+    let one = [get(0,0), get(1,1), get(2,2)].eq( &MAS ) || [get(2,2), get(1,1), get(0,0)].eq( &MAS );
+    let two = [get(2,0), get(1,1), get(0,2)].eq( &MAS ) || [get(0,2), get(1,1), get(2,0)].eq( &MAS );
+
+    if one & two { 1 } else { 0 }
+  }
+
+  pub(super) fn compute_answer( input: &str, dim: (usize, usize) ) -> usize {
+    let (rows, cols) = dim;
+    let mat = parse_input( input );
+    let mut count = 0;
+    for row in 0..rows {
+      for col in 0..cols {
+        count += count_at( &mat, row, col );
+      }
+    }
+    count
+  }
+
+  #[cfg(test)]
+  mod tests {
+    use super::*;
+    use super::super::tests::TEST_INPUT;
+
+    #[test]
+    fn test_count_at() {
+      let mat = parse_input( TEST_INPUT );
+      let count = count_at( &mat, 0, 1 );
+      assert_eq!( 1, count );
+      let count = count_at( &mat, 1, 6 );
+      assert_eq!( 1, count );
+    }
+
+    #[test]
+    fn test_compute_answer() {
+      let count = compute_answer( TEST_INPUT, (10,10) );
+      assert_eq!( 9, count );
     }
   }
 }
