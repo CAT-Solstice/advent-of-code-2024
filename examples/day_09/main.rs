e1660c9990fa063d00455d0c8fd4a56bb272ab07
solstice <solstice@archimede.edu>
2024-12-10 22:51:26 +0100

    day_09_part_two

--- examples/day_09/main.rs
+++ examples/day_09/main.rs
@@ -10,6 +10,8 @@ pub fn main() {
   let input = include_str!( "day_09.input" );
   let answer = part_one::compute_answer( input );
   println!( "{answer}" );
+  let answer = part_two::compute_answer( input );
+  println!( "{answer}" );
 }
 
 fn parse_input( input: &str ) -> Vec<Option<usize>> {
@@ -30,6 +32,13 @@ fn parse_input( input: &str ) -> Vec<Option<usize>> {
     disk
 }
 
+fn checksum( disk: &[Option<usize>] ) -> usize {
+  disk.iter().enumerate()
+    .filter_map( |(pos, elt)| elt
+      .map( |file_id| pos * file_id ))
+    .sum()
+}
+
 mod part_one {
   use super::*;
 
@@ -43,12 +52,6 @@ mod part_one {
     while let Some( empty ) = disk.iter().position( Option::is_none ) {
       disk.swap_remove( empty );
     }
-  }
-
-  fn checksum( disk: &[Option<usize>] ) -> usize {
-    disk.iter().flatten().enumerate()
-      .map( |(pos, &file_id)| pos * file_id )
-      .sum()
   }
 
   #[cfg(test)]
@@ -81,7 +84,84 @@ mod part_one {
 }
 
 mod part_two {
+  use itertools::Itertools;
+  use itertools::FoldWhile::{Continue, Done};
+  use super::*;
 
+  pub(super) fn compute_answer( input: &str ) -> usize {
+    let mut disk = parse_input( input );
+    defrag( &mut disk );
+    checksum( &disk )
+  }
+
+  fn defrag( disk: &mut [Option<usize>] ) {
+    // maybe not the best way to do it, will look for a better/simpler solution
+    let last_file_id = *disk.iter().rev().flatten().next().unwrap();
+    for src_file_id in (0..=last_file_id).rev() {
+      let (src_pos, src_len) = disk.iter().enumerate().rev()
+        .flat_map( |(idx, elt)| elt.map( |file_id| (idx, file_id) ) )
+        .skip_while( |(_, file_id)| *file_id != src_file_id )
+        .take_while( |(_, file_id)| *file_id == src_file_id )
+        .fold( None, |span, (idx,_)| span
+          .map( |(_, len)| (idx, len+1) )
+          .or( Some((idx, 1)) ))
+        .unwrap();
+
+      let (dst_pos, dst_len) = &disk[0..src_pos].iter().enumerate()
+        .filter_map( |(idx, elt)| elt.is_none().then_some(idx) )
+        .fold_while( (0, 0), |span, idx| match span {
+          (start, len) if len == src_len => Done((start, len)),
+          (start, len) if idx > start+len => Continue((idx, 1)),
+          (start, len) => Continue((start, len+1)),})
+        .into_inner();
+
+      if *dst_len == src_len {
+        for offset in 0..src_len {
+          disk.swap( src_pos+offset, dst_pos+offset );
+        }
+      }
+    }
+  }
+
+  #[cfg(test)]
+  mod tests {
+    use super::*;
+    use super::super::tests::TEST_INPUT_2;
+
+    #[test]
+    fn test_defrag() {
+      // 00992111777.44.333....5555.6666.....8888..
+      let expected = [
+        Some(0), Some(0),
+        Some(9), Some(9),
+        Some(2),
+        Some(1), Some(1), Some(1),
+        Some(7), Some(7), Some(7),
+        None,
+        Some(4), Some(4),
+        None,
+        Some(3), Some(3), Some(3),
+        None, None, None, None,
+        Some(5), Some(5), Some(5), Some(5),
+        None,
+        Some(6), Some(6), Some(6), Some(6),
+        None, None, None, None, None,
+        Some(8), Some(8), Some(8), Some(8),
+        None, None,
+      ];
+      let mut disk = parse_input( TEST_INPUT_2 );
+      defrag( &mut disk );
+      println!( "{disk:?}" );
+      assert_eq!( &expected[..], &disk[..] );
+    }
+
+    #[test]
+    fn test_compute_answer() {
+      let expected = 2858;
+      let actual = compute_answer( TEST_INPUT_2 );
+      assert_eq!( expected, actual );
+    }
+  }
 }
 
 #[cfg(test)]
